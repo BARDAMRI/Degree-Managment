ab7aa85dfafb2e2c789c9020f37edc0bb19ac097
BARDAMRI <batdam@post.bgu.ac.il>
2020-10-22 18:50:41 +0300

    remove blockers feature for now

--- DM/Backend/BL/Course.cs
+++ DM/Backend/BL/Course.cs
@@ -12,14 +12,14 @@ namespace DM.Backend.BL
         private string name;
         private int credit;
         private int grade;
-        private Dictionary<string,Course> blockers;
+      //  private Dictionary<string,Course> blockers;
 
         public Course(string name, int credit)
         {
             this.name = name;
             this.credit = credit;
             this.grade = -1;
-            blockers = new Dictionary<string, Course>();
+      //      blockers = new Dictionary<string, Course>();
 
         }
         public Course(Course cou)
@@ -27,56 +27,56 @@ namespace DM.Backend.BL
             this.name = cou.Name();
             this.credit = cou.Credit();
             this.grade = cou.Grade();
-            foreach (KeyValuePair<string, Course> co in cou.Blockers())
-            {
-                addBlocker(new Course(co.Value));
-            }
+            //foreach (KeyValuePair<string, Course> co in cou.Blockers())
+            //{
+            //    addBlocker(new Course(co.Value));
+            //}
         }
-        public Dictionary<string, Course> Blockers() => this.blockers;
+    //    public Dictionary<string, Course> Blockers() => this.blockers;
 
         public void setName(string name) 
         { 
             this.name = name; 
         }
-        public void addBlocker(Course course)
-        {
-            if (!blockers.ContainsKey(name))
-            {
-                this.blockers.Add(course.Name(), course);
-            }
-            else
-                throw new DException("blocker already exists");
-        }
-        public void removeBlocker(Course course)
-        {
-            if (isBlocker(course.Name()))
-            {
-                blockers.Remove(course.Name());
-            }
-            else
-            {
-                throw new DException("the course is not blocker for " + this.Name());
-            }
-        }
-        public void removeBlocker(string name)
-        {
-            if (isBlocker(name))
-            {
-                blockers.Remove(name);
-            }
-            else
-            {
-                throw new DException("the course is not blocker for " + this.Name());
-            }
-        }
-        public bool isBlocker(string name)
-        {
-            if(blockers.ContainsKey(name))
-            {
-                return true;
-            }
-            return false;
-        }
+        //public void addBlocker(Course course)
+        //{
+        //    if (!blockers.ContainsKey(name))
+        //    {
+        //        this.blockers.Add(course.Name(), course);
+        //    }
+        //    else
+        //        throw new DException("blocker already exists");
+        //}
+        //public void removeBlocker(Course course)
+        //{
+        //    if (isBlocker(course.Name()))
+        //    {
+        //        blockers.Remove(course.Name());
+        //    }
+        //    else
+        //    {
+        //        throw new DException("the course is not blocker for " + this.Name());
+        //    }
+        //}
+        //public void removeBlocker(string name)
+        //{
+        //    if (isBlocker(name))
+        //    {
+        //        blockers.Remove(name);
+        //    }
+        //    else
+        //    {
+        //        throw new DException("the course is not blocker for " + this.Name());
+        //    }
+        //}
+        //public bool isBlocker(string name)
+        //{
+        //    if(blockers.ContainsKey(name))
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
         public void setCredit(int newCredit)
         {
             this.credit = newCredit;
@@ -97,22 +97,22 @@ namespace DM.Backend.BL
                 Course course = (Course)obj;
                 if (this.name != course.Name() || this.credit != course.Credit())
                     equal = false;
-                foreach (KeyValuePair<string, Course> block in blockers)
-                {
-                    if(!course.hasBlocker(block))
-                    {
-                        equal = false;
-                    }
-                }
+                //foreach (KeyValuePair<string, Course> block in blockers)
+                //{
+                //    if(!course.hasBlocker(block))
+                //    {
+                //        equal = false;
+                //    }
+                //}
                 return equal;
             }
             return false;
         }
 
-        public bool hasBlocker(KeyValuePair<string, Course> block)
-        {
-            return blockers.ContainsKey(block.Key);
-        }
+        //public bool hasBlocker(KeyValuePair<string, Course> block)
+        //{
+        //    return blockers.ContainsKey(block.Key);
+        //}
 
         public override int GetHashCode()
         {
--- DM/Backend/SL/Objects/SLCourse.cs
+++ DM/Backend/SL/Objects/SLCourse.cs
@@ -12,15 +12,15 @@ namespace DM.Backend.SL.Objects
         private readonly string name;
         private readonly int credit;
         private readonly int grade;
-        private readonly IReadOnlyCollection<Course> blockers;
+    //   private readonly IReadOnlyCollection<Course> blockers;
 
-        internal SLCourse( string name, int credit, int grade, IReadOnlyCollection<Course> blockers)
+        internal SLCourse( string name, int credit, int grade)//, IReadOnlyCollection<Course> blockers)
 
         {
             this.name = name;
             this.credit = credit;
             this.grade = grade;
-            this.blockers = blockers;
+        //    this.blockers = blockers;
 
         }
         public override bool Equals(object obj)
@@ -32,14 +32,14 @@ namespace DM.Backend.SL.Objects
                 SLCourse cour = (SLCourse)obj;
                 if (cour.name == this.name & cour.credit == this.credit & cour.grade == this.grade)
                 {
-                    if (this.blockers == null & cour.blockers == null)
+                    //if (this.blockers == null & cour.blockers == null)
+                    //    return true;
+                    //IEnumerable<Course> block1 = this.blockers.Except<Course>(cour.blockers);
+                    //IEnumerable<Course> block2 = cour.blockers.Except(this.blockers);
+                    //if (block1.Count<Course>() == 0 & block2.Count<Course>() == 0)
                         return true;
-                    IEnumerable<Course> block1 = this.blockers.Except<Course>(cour.blockers);
-                    IEnumerable<Course> block2 = cour.blockers.Except(this.blockers);
-                    if (block1.Count<Course>() == 0 & block2.Count<Course>() == 0)
-                        return true;
-                    else
-                        return false;
+                    //else
+                    //    return false;
                 }
                 else
                     return false;
